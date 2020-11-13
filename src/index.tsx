69986a8637fd4124d8f5eae5ab91b370cc2b44a0
Ahmad Sadiq <ahmadsadiq885@gmail.com>
2020-11-13 14:15:52 +0530

    firebase hosting added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyADFx249HOQbc8DWRxhpH9yLCiaWcps6XU',
+  authDomain: 'letsplayit.firebaseapp.com',
+  databaseURL: 'https://letsplayit.firebaseio.com',
+  projectId: 'letsplayit',
+  storageBucket: 'letsplayit.appspot.com',
+  messagingSenderId: '500560013739',
+  appId: '1:500560013739:web:25e19546f977660338caee',
+  measurementId: 'G-2R7E2N4KYH',
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
